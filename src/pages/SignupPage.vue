c49c7f7ab6932d09bbbba2044c1b3b34b2eea843
Park Hyeon Beom <gusqja0213@naver.com>
2024-02-20 12:34:22 +0900

    test1

--- src/pages/SignupPage.vue
+++ src/pages/SignupPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="usertypecontainer">
-        <router-link class="usertypebutton" to="/signup/member">일반 회원 회원가입</router-link>
+        <router-link class="usertypebutton" to="/signup/member">일반 회원 일걸? 회원가입</router-link>
         <router-link class="usertypebutton" to="/signup/ceo">업체 회원가입</router-link>
         <router-link class="usertypebutton" to="/signup/manager">매니저 회원가입</router-link>
     </div>
